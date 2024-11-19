15ba9142b9af524a8a41c219dc24cf190b053832
Outhman mou <outhman.moustaghfir@gmail.com>
2024-11-19 22:26:48 +0100

    before getting into components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import CardList from "./components/card-list/card-list.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchValue: "",
+      num: 1,
+    };
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  onSearchEngine =(event) => {
+    console.log(event);
+    this.setState(() => {
+      return { searchValue: event.target.value };
+    });
+    console.log(this.state);
+  };
+  render() {
+    console.log("render");
+
+    const {monsters ,searchValue  } = this.state;
+    const{onSearchEngine } = this ;
+
+    const filtredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase());
+    });
+    
+    return (  
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+          <input
+            className="search-box"
+            type="search"
+            placeholder="type your search"
+            onChange={onSearchEngine}
+          />
+
+          {filtredMonsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1> {monster.name}</h1>
+              </div>
+            );
+          })}
+
+          {/* <CardList monsters = {filtredMonsters}/> */}
+
+          {/* <button onClick={() => {
+          if(this.state.name.firstName=== "Outhman"){
+            this.setState(()=>{
+              return{
+                name :{
+                  firstName: "Omar"
+                  }
+                };} , ()=>{ console.log(this.state);}) ;
+
+          }else{
+            this.setState(()=>{
+              return{
+                name :{
+                  firstName: "Outhman"
+                  }
+                  };} , ()=>{ console.log(this.state);}) ;
+          }
+          // console.log(this.state);
+          }}> change name </button> */}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
